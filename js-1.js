3b7ddada8a90bcb41c7e5aa0264f453a3fd04fe0
Ramla Jama <75176930+ramla199@users.noreply.github.com>
2022-12-14 15:39:22 +0000

    first day to change my life

---
+++ js-1.js
@@ -0,0 +1,24 @@
+// LECTURE: Values and Variables
+// 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
+let country = 'ethiopia', continent = 'any thing', population= 100000;
+// 2. Logtheirvaluestotheconsole
+console.log(country,continent,population);
+
+// LECTURE: Data Types
+// 1. Declareavariablecalled'isIsland'andsetitsvalueaccordingtoyour country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+let isIsland = false, language;
+// 2. Logthetypesof'isIsland','population','country'and'language' to the console
+console.log(typeof isIsland,typeof population, typeof language);
+
+// LECTURE: let, const and var
+// 1. Setthevalueof'language'tothelanguagespokenwhereyoulive(some countries have multiple languages, but just choose one)
+language='amharic;'
+// 2. Thinkaboutwhichvariablesshouldbeconstvariables(whichvalueswillnever change, and which might change?). Then, change these variables to const.
+// 3. Trytochangeoneofthechangedvariablesnow,andobservewhathappens
+
+
+
+
+
+
+
