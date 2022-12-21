6bcec39240cd9bf841bd0ecc919bcf048da59498
Ramla Jama <75176930+ramla199@users.noreply.github.com>
2022-12-21 20:04:08 +0000

    day 3

--- Coding-Challenge/js-a.js
+++ Coding-Challenge/js-a.js
@@ -58,6 +58,10 @@ else console.log("there's no winner ")
 // Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 // Your tasks:
 // 1. Calculatethetip,dependingonthebillvalue.Createavariablecalled'tip'for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+const bill = 275;
+let tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
+
 // 2. Printastringtotheconsolecontainingthebillvalue,thetip,andthefinalvalue (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
 // Test data:
 // § Data 1: Test for bill values 275, 40 and 430
