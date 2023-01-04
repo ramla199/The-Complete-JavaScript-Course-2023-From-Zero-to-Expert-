3f98ae1db0508a0f9efb70acaab2dd0a2d545b3e
Ramla Jama <75176930+ramla199@users.noreply.github.com>
2023-01-04 22:00:19 +0000

    "changes"

--- lecture-test/js-2.js
+++ lecture-test/js-2.js
@@ -56,27 +56,44 @@ console.log(describePopulation('India', 5467))
 
 // LECTURE: Introduction to Arrays
 // 1. Createanarraycontaining4populationvaluesof4countriesofyourchoice. You may use the values you have been using previously. Store this array into a variable called 'populations'
+const populations = [2340, 4569, 5543,3245];
 // 2. Logtotheconsolewhetherthearrayhas4elementsornot(trueorfalse)
+console.log(populations.length >= 4);
 // 3. Createanarraycalled'percentages'containingthepercentagesofthe
 // world population for these 4 population values. Use the function 'percentageOfWorld1' that you created earlier to compute the 4 percentage values
-
+const percentages = [((populations[0] * 100)/ 7900), ((populations[1] * 100)/ 7900), ((populations[2] * 100)/ 7900), ((populations[3] * 100)/ 7900)];
+console.log(percentages,populations);
 // LECTURE: Basic Array Operations (Methods)
 // 1. Createanarraycontainingalltheneighbouringcountriesofacountryofyour choice. Choose a country which has at least 2 or 3 neighbours. Store the array into a variable called 'neighbours'
+const neighbouring = ['Somalia','kenya','sudan']
 // 2. Atsomepoint,anewcountrycalled'Utopia'iscreatedintheneighbourhoodof your selected country. So add it to the end of the 'neighbours' array
+neighbouring.push('Utopia');
 // 3. Unfortunately,aftersometime,thenewcountryisdissolved.Soremoveitfrom the end of the array
+neighbouring.pop();
 // 4. Ifthe'neighbours'arraydoesnotincludethecountry‘Germany’,logtothe console: 'Probably not a central European country :D'
+console.log(!neighbouring.includes('Germany') ? 'Probably not a central European country :D' : 'central European countries');
 // 5. Changethenameofoneofyourneighbouringcountries.Todothat,findthe index of the country in the 'neighbours' array, and then use that index to change the array at that index position. For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
 //   The Complete JavaScript Course 15
-
+neighbouring.includes('kenya') ? neighbouring[neighbouring.indexOf('kenya')]  = 'Republic od kenya' : console.log('none');
+console.log(neighbouring);
 // LECTURE: Introduction to Objects
 // 1. Createanobjectcalled'myCountry'foracountryofyourchoice,containing properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments)
-
+const myCountry = {
+    country: 'Ethiopia',
+    capital: 'Addis Ababa',
+    language: 'Amharic',
+    population: 7,
+    neighbouring: ['Somalia','Kenya', 'Sudan']
+}
 // LECTURE: Dot vs. Bracket Notation
 // 1. Usingtheobjectfromthepreviousassignment,logastringlikethistothe console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.'
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbouring.length} neighbouring countries and a capital called ${myCountry.capital}.`);
 // 2. Increasethecountry'spopulationbytwomillionusingdotnotation,andthen decrease it by two million using brackets notation.
-
+myCountry.population += 2;
+console.log(myCountry.population);
 // LECTURE: Object Methods
 // 1. Addamethodcalled'describe'tothe'myCountry'object.Thismethod will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword.
+
 // 2. Callthe'describe'method
 // 3. Addamethodcalled'checkIsland'tothe'myCountry'object.This
 // method will set a new property on the object, called 'isIsland'. 'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.
